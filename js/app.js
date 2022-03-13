4c6c18323e8393f4db128593d62815b3fe21e19a
Khaled Mohamed Ibrahim <eng.khaled_ok@outlook.com>
2022-03-13 17:37:08 +0200

    Movie-App with full data

--- js/app.js
+++ js/app.js
@@ -1,50 +1,78 @@
-//DOMItems
-const smallCupsDOM = document.querySelectorAll('.cup-small');
-const litersDOM = document.getElementById('liters');
-const percentageDOM = document.getElementById('percentage');
-const remainedDOM = document.getElementById('remained');
+const API_URL = 'http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=12bb05a0d85be8d234df1c8cf3558101&page=1';
 
-updateBigCup();
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 
-//fillTheSmallCups
-smallCupsDOM.forEach((cup, index) => {
- cup.addEventListener('click', () => highlightCups(index))
-})
+const SEARCH_API = 'http://api.themoviedb.org/3/search/movie?api_key=12bb05a0d85be8d234df1c8cf3558101&query="';
 
-//functionFilling
-function highlightCups(index) {
- if (smallCupsDOM[index].classList.contains('full') && !smallCupsDOM[index].nextElementSibling.classList.contains('full')) {
-  index--;
- }
- smallCupsDOM.forEach((cup, idx) => {
-  if (idx <= index) {
-   cup.classList.add('full')
-  } else {
-   cup.classList.remove('full')
-  }
+//DomElements
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const main = document.getElementById('main');
+
+
+//Get initial movies
+getMovie(API_URL)
+
+//function_getMovie
+async function getMovie(url) {
+ const res = await fetch(url);
+ const data = await res.json()
+ showMovies(data.results)
+}
+
+//showMovie
+function showMovies(movies) {
+ main.innerHTML = '';
+ movies.forEach((movie) => {
+  const {
+   title,
+   poster_path,
+   vote_average,
+   overview
+  } = movie;
+  const movieElement = document.createElement('div');
+  movieElement.classList.add('movie');
+  movieElement.innerHTML = `
+    <!-- movie -->
+      <img src="${IMG_PATH + poster_path}" alt="${title}">
+      <!-- movie-info -->
+      <div class="movie-info">
+        <h3>${title}</h3>
+        <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+      </div>
+      <!-- end of movie-info -->
+      
+      <!-- overview -->
+      <div class="overview">
+        <h3>overview</h3>
+        ${overview}
+      </div>
+      <!-- end of overview -->
+  `
+  main.appendChild(movieElement)
  })
-
- updateBigCup()
 }
 
-function updateBigCup() {
- const fullCups = document.querySelectorAll('.full').length;
- const totalCups = smallCupsDOM.length;
-
- if (fullCups === 0) {
-  percentageDOM.style.visibility = 'hidden';
-  percentageDOM.style.height = 0;
+function getClassByRate(vote) {
+ if (vote >= 8) {
+  return 'green';
+ } else if (vote >= 5) {
+  return 'orange'
  } else {
-  percentageDOM.style.visibility = 'visible';
-  percentageDOM.style.height = `${fullCups/totalCups * 330}px`; //back to the styling of ".cup"
-  percentageDOM.innerText = `${fullCups/totalCups * 100}%`
- }
-
- if (fullCups === totalCups) {
-  remainedDOM.style.visibility = 'hidden';
-  remainedDOM.style.height = 0;
- } else {
-  remainedDOM.style.visibility = 'visible';
-  litersDOM.innerText = `${2-(250 * fullCups / 1000)}L`
+  return 'red'
  }
 }
+
+
+form.addEventListener('submit', (e) => {
+ e.preventDefault();
+ const searchTerm = search.value;
+ console.log(searchTerm);
+ if (searchTerm && searchTerm !== '') {
+  getMovie(SEARCH_API + searchTerm);
+  search.value = '';
+ } else {
+  window.location.reload();
+ }
+
+})
